18abf4f3b4bc2786239abe45a88f800436482371
shih-yao <sebasetian123@gmail.com>
2018-11-12 21:13:05 -0800

    add tabs

--- app/src/main/java/csci571/hw9/MainActivity.java
+++ app/src/main/java/csci571/hw9/MainActivity.java
@@ -1,10 +1,14 @@
 package csci571.hw9;
 
+import android.os.Build.VERSION_CODES;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
